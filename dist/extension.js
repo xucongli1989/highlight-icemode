b08a0d9172ca3e8638ad7023c77958ff0fb9297d
zhanglintc <zhanglintc623@gmail.com>
2021-04-26 12:05:31 +0800

    do not decorate current selection area

--- dist/extension.js
+++ dist/extension.js
@@ -72,6 +72,14 @@ var activate = function (context) {
                 }
                 Object.keys(decorationTypes).forEach(function (v) {
                     var range = mathes_1[v] ? mathes_1[v] : [];
+                    range = range.filter(function (o) {
+                        return !(
+                            o.range._start._line === activeEditor.selection._start._line &&
+                            o.range._end._line === activeEditor.selection._end._line &&
+                            o.range._start._character === activeEditor.selection._start._character &&
+                            o.range._end._character === activeEditor.selection._end._character
+                        );
+                    });
                     var decorationType = decorationTypes[v];
                     editor.setDecorations(decorationType, range);
                 });
